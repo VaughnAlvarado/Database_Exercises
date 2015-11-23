49c1fb6f5924c0a9de873da9390d772f1b987cb5
Vaughn <alvaradovaughn@yahoo.com>
2015-11-23 13:49:25 -0600

    where

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,18 +1,13 @@
 USE employees;
--- SELECT emp_no,hire_date,first_name,last_name
--- FROM employees
--- WHERE hire_date <= '1990';
-
--- SELECT emp_no,birth_date,first_name,last_name
--- FROM employees
--- WHERE birth_date like '%12-25%';
--- SELECT emp_no, first_name, last_name
--- FROM employees	
--- WHERE last_name like '%Q%';
-
--- WHERE (continued exercises --------->)
-SELECT emp_no, first_name, last_name
+SELECT emp_no,hire_date,first_name,last_name
 FROM employees
-WHERE first_name = 'Irena'
-OR first_name = 'Vidya'
-OR first_name = 'Maya';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT emp_no,birth_date,first_name,last_name
+FROM employees
+WHERE birth_date like '%12-25%';
+SELECT emp_no, first_name, last_name
+FROM employees	
+WHERE last_name like '%Q%';
+
+WHERE (continued exercises --------->)
